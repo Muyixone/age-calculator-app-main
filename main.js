c2172db3688d23b230c922580350b3474ba6625e
Osamuyi <muyixone@yahoo.com>
2023-06-06 14:40:28 +0100

    Add error message if the input field is empty

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-details');
+const divInput = document.getElementById('pair-input');
 const dayInput = document.getElementById('day-input');
 const monthInput = document.getElementById('month-input');
 const yearInput = document.getElementById('year-input');
@@ -12,61 +13,106 @@ const dayAgeMssg = document.getElementById('days-output');
 
 const todaysDate = new Date();
 
-form.addEventListener('input', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   const day = dayInput.value;
   const month = monthInput.value;
   const year = yearInput.value;
 
+  if (day.trim() === '' && month.trim() === '' && year.trim() === '') {
+    const errMsg = document.querySelectorAll('small');
+    for (let input of errMsg) {
+      input.innerHTML = 'This field is required';
+    }
+    AddInputErrorClass();
+    return false;
+  }
+
   const dateString = `${year}-${month}-${day}`;
 
   // Check if values inputed is valid in line with regex pattern defined
   let regex = /^\d{4}-\d{2}-\d{2}$/;
-
   if (dateString.match(regex) === null) {
-    return AddInputErrorClass();
+    return console.log('date does not match regex');
   }
 
-  return [
-    dateIsValid(dateString),
-    disableFutureDateInputs(dateString),
-    calculateAge(dateString),
-  ];
+  validateDate(dateString);
+  // return [
+  //   dateIsValid(dateString),
+  //   disableFutureDateInputs(dateString),
+  //   calculateAge(dateString),
+  // ];
 });
 
-function dateIsValid(dateString) {
-  // Create a new Date object with the inputed values(dateString) passed as a parameter
-  // Check if time stamp is valid
-  // An invalid timestamp will return NaN
-  // Check if the input passed is a valid date string by passing it
-  // Into the date.toISOString function which returns either a true or false value
-  const date = new Date(dateString);
-  const timeStamp = date.getTime();
-  /////////////////////////////////////////////////////////
-  /* BUGS TO WORK ON
-   * Figure out how to remove the 'Must be a valid day' and replace with 'Must be a valid date, when only the day input is invalid
-   * The AddInputErrorClass function does not trigger when date is invalid
-   *
-   */
-  ////////////////////////////////////////////////////////
+/*
+ * display input error message
+ */
+// function showMessage(message, type) {
+//   //get the small element and set the message
+//   const msg = document.querySelectorAll('small');
+//   msg.innerText = message;
+//   // update the class for the input
+//   msg.className = type ? 'success' : 'error';
+//   console.log(type);
+// }
 
-  if (typeof timeStamp !== 'number' || Number.isNaN(timeStamp)) {
-    dayErrMessage.innerHTML = `Must be a valid day`;
-    monthErrMessage.innerHTML = `Must be a valid month`;
-    return AddInputErrorClass();
+// function AddInputErrorClass(input, message) {
+//   return showMessage(input, message, false);
+// }
+// function clearInputError(input) {
+//   return showMessage(input, '', true);
+// }
+
+/*
+ * validate input values
+ */
+
+function validateDate(input) {
+  const date = new Date(input);
+
+  const timestamp = date.getTime();
+  if (typeof timestamp !== 'number' || Number.isNaN(timestamp)) {
+    AddInputErrorClass();
+    return false;
   }
-
-  // if (date.getDate() !== timeStamp) {
-  //   dayErrMessage.innerHTML = `Must be a valid date`;
-  //   return AddInputErrorClass();
-  // }
-  monthErrMessage.innerHTML = '';
-  dayErrMessage.innerHTML = '';
-  clearInputError();
-
-  // return dateString;
-  return date.toISOString().startsWith(dateString);
+  return date.toISOString().startsWith(input);
 }
+
+function AddInputErrorClass() {
+  for (const i of inputs) {
+    i.classList.add('error');
+  }
+}
+// function dateIsValid(dateString) {
+//   // Create a new Date object with the inputed values(dateString) passed as a parameter
+//   // Check if time stamp is valid
+//   // An invalid timestamp will return NaN
+//   // Check if the input passed is a valid date string by passing it
+//   // Into the date.toISOString function which returns either a true or false value
+
+//   const date = new Date(dateString);
+//   const timeStamp = date.getTime();
+//   /////////////////////////////////////////////////////////
+//   /* BUGS TO WORK ON
+//    * Figure out how to remove the 'Must be a valid day' and replace with 'Must be a valid date, when only the day input is invalid
+//    * The AddInputErrorClass function does not trigger when date is invalid
+//    *
+//    */
+//   ////////////////////////////////////////////////////////
+
+//   if (typeof timeStamp !== 'number' || Number.isNaN(timeStamp)) {
+//     dayErrMessage.innerHTML = `Must be a valid day`;
+//     monthErrMessage.innerHTML = `Must be a valid month`;
+//     return AddInputErrorClass();
+//   }
+
+//   monthErrMessage.innerHTML = '';
+//   dayErrMessage.innerHTML = '';
+//   clearInputError();
+
+//   // return dateString;
+//   return date.toISOString().startsWith(dateString);
+// }
 
 function disableFutureDateInputs(inputVal) {
   let year = todaysDate.getFullYear();
@@ -82,16 +128,14 @@ function disableFutureDateInputs(inputVal) {
   }
   yearErrMessage.innerHTML = '';
   clearInputError();
+  return true;
 }
 
-function AddInputErrorClass() {
-  for (const i of inputs) {
-    i.classList.add('invalid-input');
-  }
-}
 function clearInputError() {
   for (const i of inputs) {
-    i.classList.remove('invalid-input');
+    i.classList.contains('error')
+      ? i.classList.remove('error')
+      : i.classList.add('success');
   }
 }
 
